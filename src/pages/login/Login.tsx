c364f344befea22a1acf0a8b700641266c616c14
Cristhian <cristhian@hotmail.com.br>
2023-04-02 12:23:41 -0300

    pagina login

---
+++ src/pages/login/Login.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next"
+
+export const Login: NextPage = () => {
+
+    return (
+        <>
+            <h2>Login page</h2>
+            <h5>This is a login page</h5>
+        </>
+    )
+} 
