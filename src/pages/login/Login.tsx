57d8d283006d815160bf4f7f9a97ae37d623ca55
Cristhian <cristhian@hotmail.com.br>
2023-04-02 12:53:06 -0300

    form login

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,11 +1,17 @@
+import { Button, TextField } from "@mui/material"
 import { NextPage } from "next"
+import styles from './login.module.css'
 
 export const Login: NextPage = () => {
 
     return (
         <>
-            <h2>Login page</h2>
-            <h5>This is a login page</h5>
+            <div></div>
+            <div className={styles.login}>
+                <TextField required id="email" label="E-mail" />
+                <TextField required type="password" label="Senha" id="senha" />
+                <Button variant="contained" color="success">Entrar</Button>
+            </div>
         </>
     )
 } 
