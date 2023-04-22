0196bdd653270227f82cf32f328e57e3c8981641
Cristhian <carvcristhian@gmail.com>
2023-04-22 12:32:34 -0300

    mudar botao/avatar quando usuario estiver logado

--- src/components/common/header/Header.tsx
+++ src/components/common/header/Header.tsx
@@ -1,26 +1,52 @@
-import styles from './header.module.css'
-import { FunctionComponent } from "react"
-import Image from 'next/image'
-import { Button, makeStyles } from '@mui/material'
-
+import styles from "./header.module.css";
+import { FunctionComponent } from "react";
+import Image from "next/image";
+import { Button, makeStyles } from "@mui/material";
 
 export const Header: FunctionComponent = () => {
-    return (
-            <div className={styles.header}>
-                <div className={styles.header__logo}>
-                    <Image src="/logo.png" alt="logo" width="0" height="0" sizes="100vw" style={{ width: '100%', height: 'auto' }} />
-                </div>
-                <div className={styles.header__session}>
-                <div className={styles.header__login}>
-                <Button variant="contained" color="success">Login</Button>
-                <Button variant="contained" style={{
-                    backgroundColor: "#76D104",
-                    marginLeft: "0.5rem"
-                }}>Cadastro</Button>
-                </div>
+  const isLogado = true;
 
-                </div>
-
-            </div>
-    )
-} 
+  return (
+    <div className={styles.header}>
+      <div className={styles.header__logo}>
+        <Image
+          src="/logo.png"
+          alt="logo"
+          width="0"
+          height="0"
+          sizes="100vw"
+          style={{ width: "100%", height: "auto" }}
+        />
+      </div>
+      <div className={styles.header__session}>
+        {isLogado ? (
+          <div className={styles.header__usuario}>
+            <Image
+              src="/avatar.png"
+              alt="avatar"
+              width="30"
+              height="30"
+              style={{ borderRadius: "5rem", marginRight: "0.5rem"}}
+            />
+            <p>Cristhian Carvalho</p>
+          </div>
+        ) : (
+          <div className={styles.header__login}>
+            <Button variant="contained" color="success">
+              Login
+            </Button>
+            <Button
+              variant="contained"
+              style={{
+                backgroundColor: "#76D104",
+                marginLeft: "0.5rem",
+              }}
+            >
+              Cadastro
+            </Button>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
