c04ef43e50c40adf788eae639c73ab89816632ef
Cristhian <carvcristhian@gmail.com>
2023-04-26 20:37:47 -0300

    add componente layout na home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,10 @@
 import { Box, Container, Grid } from "@mui/material";
 import EventCard from "./eventcard/EventCard";
+import { Layout } from "@app/components/common/layout/Layout";
 
 export default function Home() {
     return (
+        <Layout>
         <Container maxWidth="xl" sx={{marginTop:'2rem'}}>
             <Grid container spacing={2}>
                 <Grid item md={4}>
@@ -19,5 +21,6 @@ export default function Home() {
                 </Grid>
             </Grid>
         </Container>
+        </Layout>
     )
 }
