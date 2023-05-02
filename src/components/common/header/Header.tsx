c0d39690880172bfd40039701a1dbd6478ee51e7
Cristhian <carvcristhian@gmail.com>
2023-05-02 19:49:59 -0300

    adicionei opcao de deslogar no header

--- src/components/common/header/Header.tsx
+++ src/components/common/header/Header.tsx
@@ -1,18 +1,45 @@
 import styles from "./header.module.css";
-import { FunctionComponent } from "react";
+import { FunctionComponent, useState } from "react";
 import Image from "next/image";
-import { Button } from "@mui/material";
-import { useSession } from "next-auth/react";
+import {
+  Button,
+  List,
+  ListItem,
+  ListItemText,
+  Popover,
+  Typography,
+} from "@mui/material";
+import { signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 
 export const Header: FunctionComponent = () => {
   const { data: session } = useSession();
+  const [elementoPopver, setElementoPopover] =
+    useState<HTMLImageElement | null>(null);
   const { asPath } = useRouter();
 
   const isPage = (pageName: string): boolean => {
     return asPath === `/${pageName}`;
   };
+
+  const handleAvatarClick = (
+    event: React.MouseEvent<HTMLImageElement, MouseEvent>
+  ): void => {
+    setElementoPopover(event.currentTarget);
+  };
+
+  const handleClose = (): void => {
+    setElementoPopover(null);
+  };
+
+  const handleDesconectarClick = () => {
+    signOut();
+    handleClose();
+  }
+
+  const open = Boolean(elementoPopver);
+  const id = open ? "simple-popover" : undefined;
 
   return (
     <div className={styles.header}>
@@ -37,13 +64,38 @@ export const Header: FunctionComponent = () => {
               width="30"
               height="30"
               style={{ borderRadius: "5rem", marginRight: "0.5rem" }}
+              onClick={handleAvatarClick}
             />
             {`${session.user.primeiroNome} ${session.user.segundoNome}`}
+            <Popover
+              id={id}
+              open={open}
+              anchorEl={elementoPopver}
+              onClose={handleClose}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+            >
+              <List component="div" role="group">
+                <ListItem
+                  button
+                  divider
+                  aria-haspopup="true"
+                  aria-controls="ringtone-menu"
+                  aria-label="phone ringtone"
+                  onClick={handleDesconectarClick}
+                >
+                  <ListItemText primary="Desconectar" />
+                </ListItem>
+              </List>
+            </Popover>
           </div>
         ) : (
           <div className={styles.header__login}>
             {!isPage("login") && (
-              <Button variant="contained" color="success">
+              <Button variant="contained" color="success" 
+              href="/login">
                 Login
               </Button>
             )}
