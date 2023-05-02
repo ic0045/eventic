f5b5edf28da37baa063c13019b4f186af3010a65
Cristhian <carvcristhian@gmail.com>
2023-05-01 21:36:46 -0300

    mostrar botoes header condicionalmente

--- src/components/common/header/Header.tsx
+++ src/components/common/header/Header.tsx
@@ -4,9 +4,15 @@ import Image from "next/image";
 import { Button } from "@mui/material";
 import { useSession } from "next-auth/react";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 export const Header: FunctionComponent = () => {
   const { data: session } = useSession();
+  const { asPath } = useRouter();
+
+  const isPage = (pageName: string): boolean => {
+    return asPath === `/${pageName}`;
+  };
 
   return (
     <div className={styles.header}>
@@ -26,7 +32,7 @@ export const Header: FunctionComponent = () => {
         {session ? (
           <div className={styles.header__usuario}>
             <Image
-              src={session.user.fotoPerfil ??  "/avatar.png" } 
+              src={session.user.fotoPerfil ?? "/avatar.png"}
               alt="avatar"
               width="30"
               height="30"
@@ -36,19 +42,23 @@ export const Header: FunctionComponent = () => {
           </div>
         ) : (
           <div className={styles.header__login}>
-            <Button variant="contained" color="success">
-              Login
-            </Button>
-            <Button
-              variant="contained"
-              style={{
-                backgroundColor: "#76D104",
-                marginLeft: "0.5rem",
-              }}
-              href="/cadastrousuario"
-            >
-              Cadastro
-            </Button>
+            {!isPage("login") && (
+              <Button variant="contained" color="success">
+                Login
+              </Button>
+            )}
+            {!isPage("cadastrousuario") && (
+              <Button
+                variant="contained"
+                style={{
+                  backgroundColor: "#76D104",
+                  marginLeft: "0.5rem",
+                }}
+                href="/cadastrousuario"
+              >
+                Cadastre-se
+              </Button>
+            )}
           </div>
         )}
       </div>
