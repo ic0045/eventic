6526482d3b5ea89ec2ee8223946fb88cdd09e45a
IsaqueCopque <71469931+IsaqueCopque@users.noreply.github.com>
2023-05-19 22:12:32 -0300

    fix busca titulo, add busca tipo, set busca evento ilike

--- src/pages/api/eventos/index.ts
+++ src/pages/api/eventos/index.ts
@@ -1,5 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import {  EventoRepo,  } from '@app/database'
+import { ILike } from "typeorm"
 
 /*
 *   Rota buscar eventos.
@@ -17,15 +18,16 @@ export default async function handler(
     *   Body:           None               
     */
     if(req.method === 'GET'){
-        const {id, data_inicial, destaque, categoria_id, criador_id, localizacao, titulo} = req.query;
+        const {id, data_inicial, destaque, categoria_id, criador_id, localizacao, titulo,tipo} = req.query;
         const where : { [key:string]: any} = {};
         const relations : { [key:string]: any} = {};
 
         if(id) where.id = id;
-        if(titulo) where.id = titulo;
-        if(localizacao) where.localizacao = localizacao;
+        if(titulo) where.titulo = ILike(`%${titulo}%`);
+        if(localizacao) where.localizacao = ILike(`%${localizacao}%`);;
         if(destaque) where.destaque = destaque;
         if(data_inicial) where.dataInicial = data_inicial;
+        if(tipo) where.tipo = ILike(`%${tipo}%`);;
         if(categoria_id){
             where.categoria = {id: categoria_id};
             relations.categoria = true;
@@ -34,7 +36,6 @@ export default async function handler(
             where.criador = {id: criador_id};
             relations.usuario = true;
         }
-
         try{
             const eventos = await EventoRepo.find({where: where, relations: relations});
             res.status(200).json(eventos);
